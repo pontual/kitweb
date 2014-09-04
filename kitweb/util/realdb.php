e55aea98e9305a11c62309153ac5952a0689bad6
Heitor Chang <hcproradio@gmail.com>
2014-09-04 12:17:09 -0300

    clean up

--- kitweb/util/realdb.php
+++ kitweb/util/realdb.php
@@ -1,4 +1,7 @@
 <?php
+
+// THE REAL DB FILE
+
 try {
     $db = new PDO('mysql:host=localhost;port=3306;dbname=pontualimportb1;charset=utf8', "root", "", array(
                       PDO::ATTR_PERSISTENT => true
