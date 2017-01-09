9d254dd33a10b1b02c23c85544ad7dd5bab30482
Heitor Chang <heitorchang@gmail.com>
2017-01-09 10:29:29 -0200

    begin writing new admin interface

---
+++ v2/README.txt
@@ -0,0 +1,3 @@
+Versão nova do site
+
+admin through https://pontualimportbrinde1.websiteseguro.com
---
+++ v2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Administração do Site, versão 2</title>
+    </head>
+    <body>
+        <?php
+
+        $last_updated = "9 jan 2017"
+
+        ?>
+
+        Última atualização: <?= $last_updated ?>
+    </body>
+</html>
